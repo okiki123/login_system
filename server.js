f33c6f5401a62a8fca29ecea23835f8c3d0cbbb6
okiki123 <okikimalomo17@gmail.com>
2021-12-01 00:56:07 +0100

    Updated Commit

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const bodyparser = require("body-parser");
+const session = require("express-session");
+const { v4: uuidv4 } = require("uuid");
+
+const router = require('./router.js')
+
 const app = express();
 
+const port = process.env.PORT || 3000;
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 3000
+app.use(
+  session({
+    secret: "uuidv4()",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.set("view engine", "ejs");
+app.use("/static", express.static(path.join(__dirname, "public")));
 
-app.set('view engine', 'ejs')
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/route', router);
 
-app.get('/', (req, res) => {
-    res.render('base', {title: 'Login System'})
-})
-
+app.get("/", (req, res) => {
+  res.render("base", { title: "Login System" });
+});
 
 app.listen(port, () => {
-    console.log('Listening to the server on port 3000')
-})
+  console.log("Listening to the server on port 3000");
+});
