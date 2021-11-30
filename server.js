a511a0afec0cab45a4939d66de6da82729d233a6
okiki123 <okikimalomo17@gmail.com>
2021-11-30 11:29:11 +0100

    Updated Commit

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const path = require('path')
 const app = express();
+
 
 const port = process.env.PORT || 3000
 
+app.set('view engine', 'ejs')
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('base', {title: 'Login System'})
+})
+
+
+app.listen(port, () => {
+    console.log('Listening to the server on port 3000')
+})
